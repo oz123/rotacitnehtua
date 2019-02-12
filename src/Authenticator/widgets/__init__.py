0edc4da4cc010a964b083b34457a588751ece063
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2019-02-12 11:12:38 +0100

    about dialog: no need for a specific file for it.

--- src/Authenticator/widgets/__init__.py
+++ src/Authenticator/widgets/__init__.py
@@ -16,7 +16,6 @@
  You should have received a copy of the GNU General Public License
  along with Authenticator. If not, see <http://www.gnu.org/licenses/>.
 """
-from .about import AboutDialog
 from .accounts import AccountsList, AccountRow
 from .window import Window, WindowState
 from .settings import SettingsWindow
